1981399c6ef42c0aba74f9604f69db2cc6e7fb71
Immeleo <leonardmwilu139@gmail.com>
2022-03-15 11:20:03 -0700

    c program to print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+*
+* main - Entry point
+*
+* Return:1
+*/
+int main(void)
+{
+char text_printed[9] = "_putchar";
+int count;
+
+for (count = 0; count < 9; count++)
+{
+_putchar(text_printed[count]);
+}
+_putchar('\n');
+return (0);
+}
