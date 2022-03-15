a2b7c00665cfb117ba906468c22b159edff81bf6
Immeleo <leonardmwilu139@gmail.com>
2022-03-15 12:21:29 -0700

    update file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 {
 char c = 'a';
 int i = 0;
-for (i = 0; i < 10; i++)
+while (i < 10)
 {
 while (c <= 'z')
 {
@@ -16,5 +16,6 @@ _putchar(c);
 c++;
 }
 _putchar('\n');
+i++;
 }
 }
