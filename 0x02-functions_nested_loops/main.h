2602635ac7c85a2ea912984cdee4008110817e2d
Immeleo <leonardmwilu139@gmail.com>
2022-03-15 11:50:42 -0700

    file for function declarations

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+ #define MAIN_H
+
+  /**
+   *File: main.h
+   *Auth: leonard
+   *Desc:header file containing declaration for all functions
+   *
+   */
+int _putchar(char c)
+void print_alphabet(void)
+void print_alphabet_x10(void)
+
+
+#endif
