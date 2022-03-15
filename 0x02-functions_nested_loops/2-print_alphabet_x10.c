c6bae2e6e9fc8a1c6b4154f2b9d5e0937ca6def8
Immeleo <leonardmwilu139@gmail.com>
2022-03-15 12:10:34 -0700

    c file to print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_alphabet - write a function that prints the alphabet, in lowercase x10
+*
+*/
+void print_alphabet(void)
+{
+char c = 'a';
+for (int 1 = 0; i < 10; i++)
+{
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+}
+}
