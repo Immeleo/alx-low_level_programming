063f694f65e4169bf1cf591567c61c3746ace711
Immeleo <leonardmwilu139@gmail.com>
2022-03-15 12:02:20 -0700

    update file to meet style requirements

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include "main.h"
-
-/*
+/**
 *
 * main - Entry point
 *
@@ -9,10 +8,9 @@
 */
 int main(void)
 {
-char text_printed[9] = "_putchar";
+char text_printed[8] = "_putchar";
 int count;
-
-for (count = 0; count < 9; count++)
+for (count = 0; count < 8; count++)
 {
 _putchar(text_printed[count]);
 }
