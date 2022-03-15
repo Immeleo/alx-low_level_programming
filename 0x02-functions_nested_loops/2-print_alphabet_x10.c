17bc04022d69dea435547637a92dd0a0ef049f9c
Immeleo <leonardmwilu139@gmail.com>
2022-03-15 12:13:16 -0700

    c file to print alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,10 +3,11 @@
 * print_alphabet - write a function that prints the alphabet, in lowercase x10
 *
 */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 char c = 'a';
-for (int 1 = 0; i < 10; i++)
+int i;
+for (i = 0; i < 10; i++)
 {
 while (c <= 'z')
 {
