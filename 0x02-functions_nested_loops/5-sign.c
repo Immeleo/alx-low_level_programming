5ff7518309e0fa62d74317e18e92a8d792f8a103
Immeleo <leonardmwilu139@gmail.com>
2022-03-16 13:06:10 -0700

    fxn to print sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#inlcude "main.h"
+/**
+*print_sign - function to print sign of a number
+*@int: integer
+*
+*Return: sign of number
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return(1);
+else if(n < 0)
+{
+_putchar('-');
+return(-1);
+}
+else{
+_putchar('0');
+}
+}
