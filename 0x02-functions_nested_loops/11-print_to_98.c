74b8c8a8183fe7545a544a4f427cebf0c7c71df5
Immeleo <leonardmwilu139@gmail.com>
2022-03-16 13:40:02 -0700

    fxn to print naturall numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_to_98 -print all natural nos
+*@n: integer
+*Return: natural number
+*/
+void print_to_98(int n)
+{
+if (n < 0)
+n *= -1;
+for (n >= 0; n <= 98;
+{
+return (n % 10);
+}
+}
