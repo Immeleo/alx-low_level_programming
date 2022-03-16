8137719aa20621b436693e8450754ca07b70d21d
Immeleo <leonardmwilu139@gmail.com>
2022-03-16 05:39:26 -0700

    update file

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -8,10 +8,10 @@ int _abs(int c)
 if (c < 0)
 {
 int absvalue = -c;
-_putchar("absvalue % 10" + '0');
+_putchar(absvalue % 10 + '0');
 }
 else if (c >= 0)
 {
-_putchar("c % 10" + '0');
+_putchar(c % 10 + '0');
 }
 }
