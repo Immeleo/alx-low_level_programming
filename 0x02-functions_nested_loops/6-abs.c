da54aa3d71a76c660f17cc88243381a3e58406b0
Immeleo <leonardmwilu139@gmail.com>
2022-03-16 04:58:49 -0700

    program to print absolute value of integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_abs(int): function to print the absolute value of an integer
+*
+*/
+int _abs(int c)
+{
+if (c < 0)
+{
+int absvalue = -c;
+_putchar("absvalue % 10" + '0');
+}
+else if (c >= 0)
+{
+_putchar("c % 10" + '0');
+}
+}
