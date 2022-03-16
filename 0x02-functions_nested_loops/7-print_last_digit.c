2cc30424413966c8795e9a1c6d929dab6544c308
Immeleo <leonardmwilu139@gmail.com>
2022-03-16 12:11:18 -0700

    fxn to print last digit of integer

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,10 +3,12 @@
 * print_last_digit - print last digit of an integer
 *@n: the integer to check
 *
-*Return : the last digit of a number
+*Return:last digit of number
 */
 int print_last_digit(int n)
 {
-int last_digit = (n % 10);
-return(last_digit);
+if (n < 0)
+n *= -1;
+_putchar('0' + (n % 10));
+return (n % 10);
 }
