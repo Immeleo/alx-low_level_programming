cfad0ec3e5cf75ec6a7768e5e9ee8592f6f05790
Immeleo <leonardmwilu139@gmail.com>
2022-03-16 13:57:56 -0700

    update commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,14 +12,14 @@ void print_to_98(int n)
 {
 while (n < 98)
 {
-return (n);
+_putchar((n % 10) + '0');
 n++;
 }
 while (n > 98)
 {
-return (n);
+  _putchar((n % 10) + '0');
 n--;
 }
 if (n == 98)
-return (n);
+_putchar((n % 10) + '0');
 }
